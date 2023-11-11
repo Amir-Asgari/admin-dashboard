18a21072788fba0311a21dc40ae6443cc67e60d8
amir-asgari <mr.a.asgari@gmail.com>
2023-11-11 17:21:18 +0330

    App --add setting button to the main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter, Router, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups/src";
+
+import "./App.css";
 
 function App() {
   return (
-      <h1 className='underline text-3xl  '>App</h1>
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4 " style={{ zIndex: 1000 }}>
+            <TooltipComponent content="Setting" position="top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray 
+            text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 export default App;
