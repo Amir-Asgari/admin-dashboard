c9edb394ac6ec955ccdb0987459d77207ead2ed8
amir-asgari <mr.a.asgari@gmail.com>
2023-11-25 17:18:27 +0330

    Navbar.js --add icons and navButton to the component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsChatLeft } from "react-icons/bs";
+import { RiNotification3Line } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import avatar from "../data/avatar.jpg";
+import { Cart, Chat, Notification, UserProfile } from ".";
+import { useStateContext } from "../contexts/ContextProvider";
+
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position="ButtonCenter">
+    <button >
+
+    </button>
+  </TooltipComponent>
+);
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useStateContext();
   return (
-    <div>
-        Navbar
+    <div className=" flex justify-between p-2 md:mx-6 relative">
+      <NavButton
+        title="menu"
+        customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
