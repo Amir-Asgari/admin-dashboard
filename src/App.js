e4e49351795182dbd0e6ed9c9c2b0007d3b023ad
amir-asgari <mr.a.asgari@gmail.com>
2023-11-25 16:51:47 +0330

    sidebar.js --add useStateContext and setActivemenu

--- src/App.js
+++ src/App.js
@@ -23,8 +23,11 @@ import {
   Line
 } from "./pages";
 
+import {useStateContext} from './contexts/ContextProvider'
+
 function App() {
-  const activeMenu = true;
+
+  const activeMenu = useStateContext()
 
   return (
     <div>
