553a0365a0a71ea27348ea361867b5b506be269b
amir-asgari <mr.a.asgari@gmail.com>
2023-11-27 12:21:11 +0330

    aPP.JS -remove extra navbar

--- src/App.js
+++ src/App.js
@@ -69,12 +69,6 @@ function App() {
             </div>
           </div>
 
-          <div
-            className="fixed md:static bg-main-bg
-           dark:bg-main-dark-bg navbar w-full"
-          >
-            <Navbar />
-          </div>
         </div>
         <div>
           <Routes>
