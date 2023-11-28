1fb1c998fe1f0631ea6a1e946bef04c6b341d413
amir-asgari <mr.a.asgari@gmail.com>
2023-11-28 12:34:59 +0330

    SparkLine.js --fix the problem of dosn't show the chart

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -15,8 +15,10 @@ const Ecommerce = () => {
   return (
     <div className="mt-24">
       <div className="flex flex-wrap lg:flex-nowrap justify-center w-auto">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full 
-        lg:w-100 sm:w-auto p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+        <div
+          className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full 
+        lg:w-100 sm:w-auto p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center"
+        >
           <div className="flex justify-between items-center">
             <div className="flex">
               <p className="font-bold text-gray-400">Earnings</p>
@@ -172,12 +174,12 @@ const Ecommerce = () => {
               </div>
               <div className="mt-5">
                 <SparkLine
-                  currentColor="blue"
-                  id="line-sparkline"
+                  currentColor="red"
+                  id="line-sparkLine"
                   type="Line"
                   height="80px"
                   width="250px"
-                  data="SparkLineAreaData"
+                  data={SparklineAreaData}
                   color="blue"
                 />
               </div>
