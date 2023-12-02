bc7b52fa8830713b96bcae8a9c74decce1685138
Amir_Asgari <mr.a.asgarigmail.com>
2023-12-03 01:35:40 +0330

    Orders --add grid and items map

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   GridComponent,
+  ColumnsDirective,
   ColumnDirective,
   Resize,
   Sort,
@@ -17,9 +18,18 @@ import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
 import { Header } from "../components";
 
 const Orders = () => {
-  return <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-    <Header category = 'page' title='Orders' />
-  </div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="page" title="Orders" />
+      <GridComponent>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => {
+            <ColumnDirective key={index} {...item} />;
+          })}
+        </ColumnsDirective>
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Orders;
