86b07bd96633965fa4a84c51065b062ce29fcc8b
amir-asgari <mr.a.asgari@gmail.com>
2023-12-06 16:19:42 +0330

    calender --add to the project

--- src/pages/Calender.jsx
+++ src/pages/Calender.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  DatePickerComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-calendars";
+import { scheduleData } from "../data/dummy";
+import { Header } from "../components";
+import { Schedule } from "@syncfusion/ej2-react-schedule";
 
 const Calenders = () => {
   return (
-    <div>
-      
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+     
+     Calender
+      {/* <Header category="App" title="Calender" />
+      <ScheduleComponent>
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent> */}
     </div>
-  )
-}
-
-export default Calenders
+  );
+};
+export default Calenders;
