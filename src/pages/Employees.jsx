223808ff707eb511b959c05392521de85f45d8a8
Amir_Asgari <mr.a.asgarigmail.com>
2023-12-08 01:53:07 +0330

    debug customer.js

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -14,8 +14,9 @@ import { Header } from "../components";
 const Employees = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="page" title="Employees" />
+      <Header category="Page" title="Employees" />
       <GridComponent
+        id="gridcomp"
         dataSource={employeesData}
         allowPaging
         allowSorting
@@ -27,7 +28,7 @@ const Employees = () => {
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
-        <Inject services={[Page, Search, Toolbar]} />
+          <Inject services={[Page, Search, Toolbar]} />
       </GridComponent>
     </div>
   );
